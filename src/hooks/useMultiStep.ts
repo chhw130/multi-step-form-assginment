4bc1266da5feae47f7899b57d0fe057e1f67d7e6
chhw130 <527coco@naver.com>
2025-07-15 20:03:18 +0900

    feat : useMultiStep hook

---
+++ src/hooks/useMultiStep.ts
@@ -0,0 +1,40 @@
+import { useCallback, useMemo, useState } from 'react';
+
+export type UseMultiStepProps<T extends readonly string[]> = {
+  currentStep: T[number];
+  navigateNextStep: () => void;
+  navigatePrevStep: () => void;
+  isFirstStep: boolean;
+  isLastStep: boolean;
+  stepProgress: number;
+};
+
+export const useMultiStep = <T extends readonly string[]>(steps: T): UseMultiStepProps<T> => {
+  const [currentStep, setCurrentStep] = useState(steps[0]);
+
+  const navigateNextStep = useCallback(() => {
+    if (currentStep === steps[steps.length - 1]) return;
+
+    setCurrentStep((val) => steps[steps.indexOf(val) + 1]);
+  }, [currentStep]);
+
+  const navigatePrevStep = useCallback(() => {
+    if (currentStep === steps[0]) return;
+
+    setCurrentStep((val) => steps[steps.indexOf(val) - 1]);
+  }, [currentStep]);
+
+  const isFirstStep = useMemo(() => currentStep === steps[0], [currentStep]);
+  const isLastStep = useMemo(() => currentStep === steps[steps.length - 1], [currentStep]);
+
+  const stepProgress = useMemo(() => steps.indexOf(currentStep) / steps.length, [currentStep]);
+
+  return {
+    currentStep: currentStep as T[number],
+    navigateNextStep,
+    navigatePrevStep,
+    isFirstStep,
+    isLastStep,
+    stepProgress,
+  };
+};
