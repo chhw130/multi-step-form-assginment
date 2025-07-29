fac1881de80e3ac01d09a4986355830b430cbc4a
chhw130 <527coco@naver.com>
2025-07-29 20:30:22 +0900

    chore : cache invalidation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,11 @@
 import '@/styles/globals.css';
-import { CacheProvider } from '@emotion/react';
 import type { AppProps } from 'next/app';
-import createCache from '@emotion/cache';
 import Layout from '@/components/Layout';
-
-const cache = createCache({
-  key: 'css',
-  prepend: true,
-});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <CacheProvider value={cache}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </CacheProvider>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   );
 }
